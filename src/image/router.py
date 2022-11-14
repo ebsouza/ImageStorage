fc485d7735754ca7d683b25597a0d371e851ae80
ebsouza <ebsouza07@yahoo.com>
2022-11-14 10:31:46 -0300

    error handling

--- src/image/router.py
+++ src/image/router.py
@@ -1,17 +1,21 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from starlette.responses import JSONResponse
 
 from src.image.service import get_encoded_image
 from src.image.schemas import ImageGet
+from src.image.error import ImageNotFound
 
 router = APIRouter()
 
 
 @router.get('/image')
 @router.get('/image/{image_id}')
-def get_image_view(image_id: str = None):
+def get_image_view(image_id: str):
     
-    image_ids, encoded_images = get_encoded_image(image_id)
+    try:
+        image_ids, encoded_images = get_encoded_image(image_id)
+    except ImageNotFound:
+        raise HTTPException(status_code=404, detail="Image not found")
 
     data = ImageGet.create_list(ids=image_ids, encoded_images=encoded_images)
 

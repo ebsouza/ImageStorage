e0eeb0fb51c2cbd6653dacd5144a6fa4096e51bf
ebsouza <ebsouza07@yahoo.com>
2022-11-14 14:38:01 -0300

    return tests to Dockerfile

--- src/image/utils.py
+++ src/image/utils.py
@@ -11,14 +11,12 @@ PATH_TO_IMAGE = load_config()['storage']
 IMAGE_EXTENSION = load_config()['file_extension']
 
 
-# Move to test folder
 def create_dummy_image(image_path, extension=".jpg"):
     img = Image.new('RGB', (300, 150), color='red')
     file_name = image_path + extension
     img.save(file_name)
 
 
-# Move to test folder
 def create_image_encode(image_path):
     with open(image_path, 'rb') as image:
         image_read = image.read()

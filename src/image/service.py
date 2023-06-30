81f1a9b4304c408a4fa58383bb3fe171d8bd5154
ebsouza <ebsouza07@yahoo.com>
2023-06-30 18:36:52 -0300

    feat: logging create and delete events

--- src/image/service.py
+++ src/image/service.py
@@ -2,17 +2,23 @@ from typing import List
 
 from src.image.model import Image
 from src.image.repository import ImageRepository
+from src.logging import get_logging
+
+logger = get_logging(__name__)
 
 
 async def create_image(image: Image, repository: ImageRepository):
     await repository.add(image)
+    logger.info(f"Image '{image.id}' was sucessfully created")
 
 
 def remove_image(image_id: str, repository: ImageRepository) -> List[str]:
     if image_id is None:
         image_ids = repository.remove_all()
+        logger.info("All images were sucessfully removed")
     else:
         repository.remove(image_id)
+        logger.info(f"Image '{image_id}' was sucessfully removed")
         image_ids = [image_id]
     return image_ids
 

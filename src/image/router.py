43f0e9d42401530a94240f3856cc7068821346a2
ebsouza <ebsouza07@yahoo.com>
2023-08-11 11:11:14 -0300

    feat: update schemas

--- src/image/router.py
+++ src/image/router.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter
 from starlette.responses import JSONResponse
 
-import src.image.schemas as schema
-from src.image.schemas import ImageIn
+from src.image.schemas import ImageIn, ImageOut, ImageManyOut
 #from src.image.model import Image
 from src.image.repository import ImageRepository
 from src.image.service import (create_image, get_image_many, get_image, remove_image)
@@ -15,26 +14,23 @@ def build_images_router(image_repository: ImageRepository):
     @router.get('/')
     def get_images_view(offset: int = 0, limit: int = 10):
         images = get_image_many(image_repository, offset, limit)
-        data = schema.build_schema(images, offset, limit)
+        data = ImageManyOut(images, offset, limit).data
 
         return JSONResponse(content=data, status_code=200)
 
     @router.get('/{image_id}')
     def get_image_view(image_id: str):
         image = get_image(image_id, image_repository)
-
-        if image is None:
-            data = {}
-        else:
-            data = image.as_dict()
+        data = ImageOut.from_model_to_schema(image)
 
         return JSONResponse(content=data, status_code=200)
 
     @router.post('/')
     def create_image_view(image: ImageIn):
-        image_id = create_image(image.data, image_repository)
+        image = create_image(image.data, image_repository)
+        data = ImageOut.from_model_to_schema(image)
 
-        return JSONResponse(content={'image_id': image_id}, status_code=201)
+        return JSONResponse(content=data, status_code=201)
 
     @router.delete('/{image_id}')
     def remove_image_view(image_id: str):
--- src/image/schemas.py
+++ src/image/schemas.py
@@ -13,53 +13,62 @@ class ImageIn(BaseModel):
 
 
 class ImageOut(BaseModel):
-    data: str
+    id: int
+    path: str
+
+    @classmethod
+    def from_model_to_schema(cls, image: Image):
+        if image is None:
+            return {}
+
+        return cls(id=image.id, path=image.path).model_dump()
 
 
-def build_schema(images: List[Image], offset: int, limit: int):
-    return get_image_collection(images, offset, limit)
+class ImageManyOut:
+
+    INVALID_OFFSET = -1
+
+    def __init__(self, images: List[Image], offset: int, limit: int):
+        self.data = self._get_image_collection(images, offset, limit)
+
+    def _get_image_collection(self, images: List[Image], offset: int = 0, limit: int = 10):
+        next_offset = self._get_next_offset(offset, limit, len(images))
+        previous_offset = self._get_previous_offset(offset, limit)
+
+        data = {
+            "kind": "Collection",
+            "next": self._build_link(next_offset, limit),
+            "previous": self._build_link(previous_offset, limit),
+            "data": list()
+        }
+
+        for image in images:
+            content = {'id': image.id, 'path': image.path}
+            data["data"].append(content)
+
+        return data
+    
+    def _get_next_offset(self, offset: int, limit: int, total_images: int):
+        next_offset = offset + limit
+
+        if total_images < limit:
+            next_offset = self.INVALID_OFFSET
+
+        return next_offset
 
 
-def get_image_collection(images: List[Image], offset: int = 0, limit: int = 10):
-    next_offset = get_next_offset(offset, limit, len(images))
-    previous_offset = get_previous_offset(offset, limit)
+    def _get_previous_offset(self, offset: int, limit: int):
+        previous_offset = offset - limit
 
-    data = {
-        "kind": "Collection",
-        "next": build_link(next_offset, limit),
-        "previous": build_link(previous_offset, limit),
-        "data": list()
-    }
+        if previous_offset <= 0:
+            previous_offset = 0
 
-    #for image in images[offset:offset + limit]:
-    for image in images:
-        content = {'id': image.id, 'path': image.path}
-        data["data"].append(content)
-
-    return data
+        return previous_offset
 
 
-def get_next_offset(offset: int, limit: int, total_images: int):
-    next_offset = offset + limit
+    def _build_link(self, offset: int, limit: int):
+        if offset == self.INVALID_OFFSET:
+            return None
 
-    if total_images < limit:
-        next_offset = INVALID_OFFSET
-
-    return next_offset
-
-
-def get_previous_offset(offset: int, limit: int):
-    previous_offset = offset - limit
-
-    if previous_offset <= 0:
-        previous_offset = 0
-
-    return previous_offset
-
-
-def build_link(offset: int, limit: int):
-    if offset == INVALID_OFFSET:
-        return None
-
-    HOST = os.getenv('EXPOSED_URL')
-    return f'{HOST}/v1/images?offset={offset}&limit={limit}'
+        HOST = os.getenv('EXPOSED_URL')
+        return f'{HOST}/v1/images?offset={offset}&limit={limit}'
